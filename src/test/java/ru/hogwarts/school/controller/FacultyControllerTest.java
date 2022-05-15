9f1436099b89aed64cd19ea27a2fa526fb41a2b9
Olga22S <22031992Les>
2022-05-15 23:59:30 +0300

    tests for faculty

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -1,16 +1,18 @@
 package ru.hogwarts.school.controller;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.service.FacultyService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FacultyController.class)
@@ -24,11 +26,31 @@ class FacultyControllerTest {
 
     @Test
     public void testGetFaculty() throws Exception {
-        Faculty faculty = new Faculty();
-        when(service.get(any(Long.class))).thenReturn(faculty);
+        when(service.get(any(Long.class))).thenReturn(new Faculty());
+
         mockMvc.perform(
-                        get("/hogwarts/faculty/{id}", 1))
+                        get("/faculty/{id}", 1))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void testDeleteFaculty() throws Exception {
+        mockMvc.perform(
+                        delete("/faculty/{id}", 1))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddFaculty() throws Exception {
+        when(service.add(any(Faculty.class))).thenReturn(new Faculty());
+        JSONObject userObject = new JSONObject();
+
+        mockMvc.perform(
+                        post("/faculty")
+                                .content(userObject.toString())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
 
 }
