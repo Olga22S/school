7cb21ac5021e5dfa5c9a81d478f13198a169a1ac
Olga22S <22031992Les>
2022-05-20 00:09:17 +0300

    changed previous

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Src;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -10,15 +11,15 @@ import java.util.List;
 
 public interface AvatarService {
 
-    ResponseEntity<Void> uploadAvatar(Long id, MultipartFile avatar) throws IOException;
+    void uploadAvatar(Long id, MultipartFile avatar) throws IOException;
 
     Avatar getAvatarById(Long id);
+
+    ResponseEntity<byte[]> downloadAvatar(Long id, Src src, HttpServletResponse response) throws IOException;
 
     ResponseEntity<byte[]> downloadFromDataBase(Long id);
 
     ResponseEntity<byte[]> downloadFromLocalDisk(Long id, HttpServletResponse response) throws IOException;
-
-    ResponseEntity<byte[]> downloadAvatar(Long id, Enum src, HttpServletResponse response) throws IOException;
 
     List<Avatar> getAllAvatars(Integer pageNumber, Integer pageSize);
 }
