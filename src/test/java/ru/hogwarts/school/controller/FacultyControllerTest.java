455604770ad5acf715826635261d98c22349dfb8
Olga22S <22031992Les>
2022-05-23 22:20:41 +0300

    hw 4.2 is done

---
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -0,0 +1,163 @@
+package ru.hogwarts.school.controller;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(FacultyController.class)
+class FacultyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FacultyService service;
+
+    @Test
+    public void testGetFaculty() throws Exception {
+        Long id = 1L;
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        when(service.get(id)).thenReturn(faculty);
+
+        mockMvc.perform(
+                        get("/faculty/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id));
+    }
+
+    @Test
+    public void testDeleteFaculty() throws Exception {
+        mockMvc.perform(
+                        delete("/faculty/{id}", 1))
+                .andExpect(status().isOk());
+        verify(service).remove(1L);
+    }
+
+    @Test
+    public void testAddFaculty() throws Exception {
+        Long id = 1L;
+        String name = "Nice";
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setName(name);
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", id);
+        userObject.put("name", name);
+
+        when(service.add(any(Faculty.class))).thenReturn(faculty);
+
+        mockMvc.perform(
+                        post("/faculty")
+                                .content(userObject.toString())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+        verify(service).add(faculty);
+    }
+
+    @Test
+    public void testUpdateFaculty() throws Exception {
+        Long id = 1L;
+        String name = "Nice";
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setName(name);
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", id);
+        userObject.put("name", name);
+
+        when(service.update(any(Faculty.class))).thenReturn(faculty);
+
+        mockMvc.perform(
+                        put("/faculty")
+                                .content(userObject.toString())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+        verify(service).update(faculty);
+    }
+
+    @Test
+    public void testGetAllFaculties() throws Exception {
+        when(service.getAll()).thenReturn(List.of(new Faculty()));
+
+        mockMvc.perform(
+                        get("/faculty/all"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetFacultyByColor() throws Exception {
+        Long id = 1L;
+        String color = "красный";
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setColor(color);
+        List<Faculty> faculties = List.of(faculty);
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", id);
+        userObject.put("color", color);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(userObject);
+
+        when(service.getByColor(color)).thenReturn(faculties);
+
+        mockMvc.perform(
+                        get("/faculty/color?color=красный"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonArray.toString()));
+    }
+
+    @Test
+    public void testGetFacultyByName() throws Exception {
+
+        Faculty faculty = new Faculty();
+        faculty.setId(1L);
+        faculty.setName("Nice");
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", 1L);
+        userObject.put("name", "Nice");
+
+        when(service.getFacultyByNameIgnoreCase("Nice")).thenReturn(faculty);
+
+        mockMvc.perform(
+                        get("/faculty/name?name=Nice"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+    }
+
+    @Test
+    public void testGetFacultyByStudentId() throws Exception {
+        Long id = 1L;
+        String name = "Nice";
+        Faculty faculty = new Faculty();
+        faculty.setId(id);
+        faculty.setName(name);
+        JSONObject userObject = new JSONObject();
+        userObject.put("id", id);
+        userObject.put("name", name);
+
+        when(service.getFacultyByStudentId(any(Long.class))).thenReturn(faculty);
+
+        mockMvc.perform(
+                        get("/faculty/student?id=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(userObject.toString()));
+    }
+}
---
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -0,0 +1,113 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class StudentControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() {
+        assertThat(studentController).isNotNull();
+    }
+
+    @Test
+    public void testGetStudent() {
+        ResponseEntity<Student> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/hogwarts/student/1", Student.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().getId());
+    }
+
+    @Test
+    public void testGetAllStudents() {
+        ResponseEntity<Student[]> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/hogwarts/student/all", Student[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testGetStudentByAge() {
+        ResponseEntity<Student[]> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/hogwarts/student/age?age=21", Student[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(Arrays.stream(response.getBody())
+                .allMatch(student -> student.getAge() == 21));
+    }
+
+    @Test
+    public void testGetStudentsByAgeBetween() {
+        ResponseEntity<Student[]> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/hogwarts/student/age/?min=21&max=22", Student[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(Arrays.stream(response.getBody())
+                .allMatch(student -> student.getAge() == 21 || student.getAge() == 22));
+    }
+
+    @Test
+    public void testGetStudentsByFacultyId() {
+        ResponseEntity<Student[]> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/hogwarts/student/faculty?id=1", Student[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddStudent() {
+        Student student = new Student();
+        student.setName("Kirill");
+
+        ResponseEntity<Student> response = restTemplate.postForEntity(
+                "http://localhost:" + port + "/hogwarts/student", student, Student.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().getName().equals("Kirill"));
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        Student student = new Student();
+        HttpEntity<Student> entity = new HttpEntity(student);
+
+        ResponseEntity<Student> response = restTemplate.exchange(
+                "http://localhost:" + port + "/hogwarts/student", HttpMethod.PUT, entity, Student.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteStudent() {
+        Student student = new Student();
+        HttpEntity<Student> entity = new HttpEntity(student);
+
+        ResponseEntity<Student> response = restTemplate.exchange(
+                "http://localhost:" + port + "/hogwarts/student/1", HttpMethod.DELETE, entity, Student.class);
+        assertThat(response.getStatusCode().equals(HttpStatus.OK));
+    }
+}
