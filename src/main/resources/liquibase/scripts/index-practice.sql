f6c74e7d1b32f08569b11a0091702ebdeb86a2e5
Olga22S <22031992Les>
2022-05-26 22:26:33 +0300

    hw 4.3

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,6 +1,5 @@
---liquibase formatted sql
+-- liquibase formatted sql
 
---changeSet olga:1
+-- changeSet olga:1
 CREATE INDEX student_name_index ON student (name);
---changeSet olga:2
 CREATE INDEX faculty_name_color ON faculty (name, color);
